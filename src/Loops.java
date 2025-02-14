6e87f171f3c6e2737a117b7998a9bd188aa1cab9
junlee <jun.lee73@outlook.com>
2025-02-14 11:39:12 +1100

    Finished Loops practice questions

--- src/Loops.java
+++ src/Loops.java
@@ -1,11 +1,61 @@
+import jets.Jets;
+
+import java.util.Random;
+
 public class Loops {
     public static void main(String[] args) {
-        int integers = 1000;
-        while (integers <= 2000) {
-            if(integers % 5 == 0 && integers % 7 != 0) {
-                System.out.println(integers);
+        //Fizzbuzz
+        for (int i = 0; i <= 50; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz!");
+            } else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
-            integers++;
+        }
+
+        //Triangle thing
+        String layer = "";
+        for (int i = 0; i <= 5 ; i++) {
+            layer += "*";
+            System.out.print(layer);
+            System.out.println("");
+        }
+
+        int n = Jets.inputInt("Enter the index of the fibbonaci sequence");
+        System.out.println(fibonacci(n));
+
+        String name = Jets.inputString("Enter your name");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print(name.charAt(name.length() - 1 - i));
+        }
+
+        //Number Guessing Game
+        Random rand  = new Random();
+        int num = rand.nextInt(50);
+        boolean win = false;
+
+        while(!win) {
+            int guess = Jets.inputInt("Enter a number");
+            if (guess == num) {
+                win = true;
+            } else if (guess > num) {
+                System.out.println("You are higher");
+            } else if (guess < num) {
+                System.out.println("You are lower");
+            }
+        }
+        System.out.println("You win!");
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
