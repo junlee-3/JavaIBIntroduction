dc7a9de7f4cabba8cc395c20017a1f82f08bf4be
junlee <jun.lee73@outlook.com>
2025-03-24 12:43:46 +1100

    i like lists

--- src/collectionsAndLists.java
+++ src/collectionsAndLists.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.*;
+import jets.Jets;
 
 public class collectionsAndLists {
     public static void main(String[] args) {
+        lists();
+
+    }
+
+    public static void arrayTest() {
         int indexEnd = 0;
         int arr[] = new int[26];
         for (int i = 0; i < 52; i+=2) {
@@ -23,5 +31,32 @@ public class collectionsAndLists {
         }
         System.out.println(Arrays.toString(arr));
 
+        for(int i = 0; i < indexEnd; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void lists() {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i <= 50; i+=2) {
+            list.add(i);
+        }
+        System.out.println(list);
+
+        for (int i = 0; i <= 10; i++) {
+            list.remove(i);
+        }
+        System.out.println(list);
+
+        list.remove(4);
+        System.out.println(list);
+
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+
+        list.clear();
+        System.out.println(list);
     }
 }
